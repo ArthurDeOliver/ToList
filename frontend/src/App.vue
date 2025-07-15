bf617ffcafa7e64dea6fb44241d08cabd84b7256
ArthurAgys <arthur@agys.com.br>
2025-07-15 15:55:44 -0300

    ˜teste˜

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,27 @@
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent } from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+})
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div>
+      <a href="https://vite.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+    <HelloWorld msg="Arthur" />
   </div>
-  <HelloWorld msg="Arthur :D" />
 </template>
 
 <style scoped>
